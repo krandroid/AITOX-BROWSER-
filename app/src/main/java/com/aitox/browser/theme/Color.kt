c1335e7e70c44fadd41e1f1033ef85bef7f78c55
krandroid <krandroid@gmail.com>
2026-06-18 11:25:20 +0700

    Add theme colors

---
+++ app/src/main/java/com/aitox/browser/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.aitox.browser.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000)
+val DarkGray = Color(0xFF121212)
+val Gray = Color(0xFF1E1E1E)
+val LightGray = Color(0xFF2A2A2A)
+val White = Color(0xFFFFFFFF)
+val Purple = Color(0xFFBB86FC)
+val Teal = Color(0xFF03DAC5)
